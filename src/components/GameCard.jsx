1e55c79f009468f64e11f56ac88a5dc20522d5bb
David Rodriguez Gonzalez <davidrodriguezgonzalez@Davids-MacBook-Air.local>
2026-03-25 02:00:27 +0100

    fix(image card)

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -17,7 +17,6 @@ const GameCard = ({ song, showDetails, isWrong }) => {
         {/* CARA TRASERA */}
         <div className="card-face card-face-back">
           <div className="song-info">
-            <img src={song.image} alt="Album cover" className="album-cover" />
             <h2 className="song-title">{song.title}</h2>
             <h3 className="song-artist">{song.artist}</h3>
             <div className="year-badge">{song.year}</div>
